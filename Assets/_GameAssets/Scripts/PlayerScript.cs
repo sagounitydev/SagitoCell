e37f0fe27ee431272befe64552017af13a4eb234
sagounitydev <sago.unitydev@gmail.com>
2018-11-13 13:42:59 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour {
+
+    public GameObject targetCircle;
+    Animator animator;
+    NavMeshAgent agente;
+    
+    private void Start() {
+        agente = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            ManageMouseClick();
+        }
+    }
+
+    private void ManageMouseClick() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rch;
+        bool hasTouch = Physics.Raycast(ray, out rch);
+        if (hasTouch) {
+            targetCircle.transform.position = rch.point;
+            targetCircle.transform.rotation = Quaternion.LookRotation(rch.normal);
+            agente.destination = targetCircle.transform.position;
+            animator.SetBool("Andando", true);
+        }
+    }
+
+}
