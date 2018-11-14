da1372c0b79c34990d2936ae840f7d966ab88698
sagounitydev <sago.unitydev@gmail.com>
2018-11-14 11:33:28 +0100

    Act 14 Nov

---
+++ Assets/_GameAssets/Scripts/KeyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    [SerializeField] GameObject Puerta;
+    [SerializeField] GameObject generadorItem;
+    [SerializeField] ParticleSystem ItemDestroy;
+    [SerializeField] GameObject generadorPuerta;
+    [SerializeField] ParticleSystem psPuerta;
+    [SerializeField] AudioSource audioBoom;
+    public Animator animatorPuerta;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            AbrirPuerta();
+            audioBoom.Play();
+            ParticleSystem psItem = Instantiate(ItemDestroy, generadorItem.transform.position, Quaternion.identity);
+            ParticleSystem psDoor = Instantiate(psPuerta, generadorPuerta.transform.position, Quaternion.identity);
+            psItem.Play();
+            psDoor.Play();
+            Destroy(this.gameObject);
+        }        
+    }
+
+    private void AbrirPuerta() {
+        animatorPuerta.SetBool("AbreteSesamo", true);
+    }
+}
--- Assets/_GameAssets/Scripts/PlayerScript.cs
+++ Assets/_GameAssets/Scripts/PlayerScript.cs
@@ -5,10 +5,16 @@ using UnityEngine.AI;
 
 public class PlayerScript : MonoBehaviour {
 
+    enum Estado { Idle, Andando, Corriendo, Saltando, Disparando};
+    Estado estado = Estado.Idle;
+
     public GameObject targetCircle;
+    //Filtro de capas
+    public LayerMask walkableLayer;
+
     Animator animator;
     NavMeshAgent agente;
-    
+        
     private void Start() {
         agente = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
@@ -18,21 +24,64 @@ public class PlayerScript : MonoBehaviour {
         if (Input.GetButtonDown("Fire1")) {
             ManageMouseClick();
         }
-        if (agente.remainingDistance <= agente.stoppingDistance) {
-            animator.SetBool("Andando", false);
+
+        switch (estado) {
+            case Estado.Idle:
+                //NO HAGO NADA
+                break;
+            case Estado.Andando:
+                ComprobarDestino();
+                break;
+            case Estado.Saltando:
+
+                break;
+            case Estado.Corriendo:
+
+                break;
+            case Estado.Disparando:
+
+                break;
         }
+    }
+
+    private void ComprobarDestino() {
+        if (!agente.pathPending) {
+            if (agente.remainingDistance <= agente.stoppingDistance) {
+                animator.SetBool("Andando", false);
+                estado = Estado.Idle;
+            }
+        }     
     }
 
     private void ManageMouseClick() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit rch;
-        bool hasTouch = Physics.Raycast(ray, out rch);
+        bool hasTouch = Physics.Raycast(ray, out rch, Mathf.Infinity, walkableLayer);
         if (hasTouch) {
-            targetCircle.transform.position = rch.point;
-            targetCircle.transform.rotation = Quaternion.LookRotation(rch.normal);
-            agente.destination = targetCircle.transform.position;
-            animator.SetBool("Andando", true);
+            switch (estado) {
+                case Estado.Idle:
+                    Andar(rch);
+                    break;
+                case Estado.Andando:
+
+                    break;
+                case Estado.Saltando:
+
+                    break;
+                case Estado.Corriendo:
+
+                    break;
+                case Estado.Disparando:
+
+                    break;
+            }            
         }
     }
-
+    private void Andar(RaycastHit rch) {
+        targetCircle.transform.position = rch.point;
+        targetCircle.transform.rotation = Quaternion.LookRotation(rch.normal);
+        agente.destination = targetCircle.transform.position;
+        animator.SetBool("Andando", true);
+        estado = Estado.Andando;
+    }
 }
